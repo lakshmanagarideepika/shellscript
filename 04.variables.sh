8a2ebd4ebc8457ae23157c82b9ab208097cd65aa
lakshmanagarideepika <deepikareddy423@gmail.com>
2023-12-16 19:55:07 +0530

    added 2nd script

---
+++ 04.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hello $PERSON2, Good Morning"
+echo "$PERSON2: hi $PERSON1, very Good Morning"
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: I am good $PERSON1, how are you"
---
+++ 05.variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "please enter your username::"
+
+read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
+
+echo "please enter your password::"
+read -s PASSWORD
+
+echo "Username is : $USERNAME, Password is : $PASSWORD"  # iam printing just for validation, you should not print username 
+   #and passwords in scripts
+
+
+
+
